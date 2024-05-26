979fa05e967dc1d5eeb1052dc511079710b8ecab
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2024-05-26 17:32:23 -0300

    comeco spotify

--- script.js
+++ script.js
@@ -404,9 +404,6 @@ function play(){
         comecaTempo();
     }
 }
-function teste(){
-    location.href = "https://accounts.spotify.com/authorize?response_type=code&client_id=2d139ecf9644474eb0f8f9d2afbac698&scope=playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public&redirect_uri=https://iampedroeduardo.github.io/cisum/game.html";
-}
 var artistas = [
     new Artista("Taylor Swift"),
     new Artista("Jão"),
