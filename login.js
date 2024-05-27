13e65928e2d8136fe4dc6eb52acedde15c451871
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2024-05-27 17:13:32 -0300

    spotify

--- login.js
+++ login.js
@@ -1,3 +1,3 @@
 function login(){
-    location.href = "https://accounts.spotify.com/authorize?response_type=code&client_id=2d139ecf9644474eb0f8f9d2afbac698&scope=playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public&redirect_uri=https://iampedroeduardo.github.io/cisum/game.html";
+    location.href = "https://accounts.spotify.com/authorize?response_type=token&client_id=2d139ecf9644474eb0f8f9d2afbac698&scope=playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public&redirect_uri=https://iampedroeduardo.github.io/cisum/game.html";
 }
