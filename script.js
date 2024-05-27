6705da77d9a661de41692c4863c7b17eb2319821
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2024-05-27 15:53:05 -0300

    spotify

--- script.js
+++ script.js
@@ -421,7 +421,7 @@ function pegaCodigoSpotify(){
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": "true",
             "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Accept, Accept-Language, Content-Language, Content-Type"
         }
     };
     fetch("https://accounts.spotify.com/api/token", api)
