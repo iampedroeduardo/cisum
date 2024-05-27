0545d49ac1e32676bdf50e8efa4debc48558cb9f
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2024-05-27 15:54:53 -0300

    spotify

--- script.js
+++ script.js
@@ -420,7 +420,7 @@ function pegaCodigoSpotify(){
             "Content_Type": "application/x-www-form-urlencoded",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": "true",
-            "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+            "Access-Control-Allow-Methods": "POST",
             "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Accept, Accept-Language, Content-Language, Content-Type"
         }
     };
